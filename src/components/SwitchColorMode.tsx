0b6167dd529c9a3dd2ca4ae83e098ae520ca4e28
arashjp8 <arashjafarpour723@gmail.com>
2024-06-02 20:06:37 +0330

    [feature] - changing the switch light mode button

--- src/components/SwitchColorMode.tsx
+++ src/components/SwitchColorMode.tsx
@@ -1,17 +1,15 @@
-import { MoonIcon } from "@chakra-ui/icons";
-import { HStack, Icon, Switch, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Button, HStack, Icon, useColorMode } from "@chakra-ui/react";
 
 function SwitchColorMode() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack spacing={2}>
-      <Icon fontSize={"xl"}>{<MoonIcon />}</Icon>
-      <Switch
-        isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
-        size={"lg"}
-        isFocusable
-      />
+      <Button onClick={toggleColorMode} variant={"solid"} width={"20px"} height={"35px"} borderRadius={"10px"}>
+        <Icon fontSize={"xl"}>
+          {colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+        </Icon>
+      </Button>
     </HStack>
   );
 }
