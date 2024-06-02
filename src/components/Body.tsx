9a05492f2e07909576e01f106cefa235e92f680b
arashjp8 <arashjafarpour723@gmail.com>
2024-06-02 19:06:15 +0330

    [checkpoint] - bolbol photo added to hero section

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,20 +1,20 @@
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import { Heading, Image, Text, VStack } from "@chakra-ui/react";
+import Bolbol from "../assets/bolbol.svg";
 
 function Body() {
   return (
-    <VStack
-      paddingX={4}
-      paddingY={2}
-      height={"100vh"}
-      width={"100%"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      spacing={4}
-    >
-      <Heading>خریدی مطمئن با بلبل نشان</Heading>
-      <Text fontSize={"xl"} maxWidth={{ sm: "300px" }} textAlign={"center"}>
-        ما بهترین را برای شما انتخاب کرده ایم همین حالا خرید کنید
-      </Text>
+    <VStack height={"100vh"} width={"100%"} spacing={6} justifyContent={"center"} alignItems={"center"}>
+      <Image src={Bolbol} alt={"Bolbol"} />
+      <VStack
+        paddingX={4}
+        paddingY={2}
+        spacing={4}
+      >
+        <Heading>خریدی مطمئن با بلبل نشان</Heading>
+        <Text fontSize={"xl"} maxWidth={{ sm: "300px" }} textAlign={"center"}>
+          ما بهترین را برای شما انتخاب کرده ایم همین حالا خرید کنید
+        </Text>
+      </VStack>
     </VStack>
   );
 }
