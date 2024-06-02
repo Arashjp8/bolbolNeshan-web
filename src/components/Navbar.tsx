d33db7a3cb02d56f38693c1d0df07ba9afa5302a
arashjp8 <arashjafarpour723@gmail.com>
2024-06-02 20:40:12 +0330

    [refactor] - removing switch color mode component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import { HStack, Image } from "@chakra-ui/react";
-import SwitchColorMode from "./SwitchColorMode";
+import { Button, HStack, Icon, Image, useColorMode } from "@chakra-ui/react";
 import Logo from "../assets/bolbol.svg";
 import NavigationOptions from "./NavigationOptions";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack
       paddingX={4}
@@ -13,7 +14,18 @@ function Navbar() {
       width={"100%"}
       minWidth={"400px"}
     >
-      <SwitchColorMode />
+      <Button
+        onClick={toggleColorMode}
+        variant={"solid"}
+        width={"20px"}
+        height={"35px"}
+        borderRadius={"10px"}
+      >
+        <Icon fontSize={"xl"}>
+          {colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+        </Icon>
+      </Button>
+
       <NavigationOptions />
       <Image src={Logo} alt="Logo" width={"50px"} height={"50px"} />
     </HStack>
