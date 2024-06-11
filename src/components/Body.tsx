182202aae44e40a915c894282b007d9e4f6adcc4
arashjp8 <arashjafarpour723@gmail.com>
2024-06-11 19:06:39 +0330

    [checkpoint] - dist folder added

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,13 +3,15 @@ import Bolbol from "../assets/bolbol.svg";
 
 function Body() {
   return (
-    <VStack height={"100vh"} width={"100%"} spacing={6} justifyContent={"center"} alignItems={"center"}>
+    <VStack
+      height={"100vh"}
+      width={"100%"}
+      spacing={6}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
       <Image src={Bolbol} alt={"Bolbol"} />
-      <VStack
-        paddingX={4}
-        paddingY={2}
-        spacing={4}
-      >
+      <VStack paddingX={4} paddingY={2} spacing={4}>
         <Heading>خریدی مطمئن با بلبل نشان</Heading>
         <Text fontSize={"xl"} maxWidth={{ sm: "300px" }} textAlign={"center"}>
           ما بهترین را برای شما انتخاب کرده ایم همین حالا خرید کنید
